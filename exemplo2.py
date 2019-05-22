16ce294d4eeb9cbbac312ee2fcc35dd36ad0c9f6
duBorgis <duborgis@gmail.com>
2019-05-22 19:27:33 -0300

    SEGUNDA AULA

--- exemplo2.py
+++ exemplo2.py
@@ -10,16 +10,17 @@ m = GEKKO(remote=True)
 xA = m.Var(value=1, lb=0, ub=1)
 xB = m.Var(value=0, lb=0, ub=1)
 xC = m.Var(value=0, lb=0, ub=1)
-T = m.MV(value=298, lb=273, ub=373 )
-T.STATUS = 1 #LIBERA A VARIAVEL PARA O APMONITOR MEXER
-T.DMAX = 2
-T.DCOST = 0.5
+Tf = m.Var(value=0.1, lb=0.1, ub=100)
+# T = m.MV(value=298, lb=273, ub=373 )
+# T.STATUS = 1 #LIBERA A VARIAVEL PARA O APMONITOR MEXER
+# T.DMAX = 2
+# T.DCOST = 0.5
 #definir parametros do modelo
 k01=4e3
 k02=6.2e5
 E1=2500
 E2=5000
-# T=350
+T=300
 #Criar Intermediates
 #AVALIADO QUANDO RESOLVIDO O SISTEMA
 #utilizar funç~eso configuradas no GEKKO, não usar math ou numpy
@@ -40,14 +41,15 @@ k2 = m.Intermediate(k02*m.exp(-E2/T))
 # m.Equations(eqs)
 # OUTRA FORMA DE DEFINIR EQUAÇÕES
 def func():
-	f1 = k1*xA**2 - k2*xB
-	f2 = -k1*xA**2
+	f1 = Tf*(k1*xA**2 - k2*xB)
+	f2 = Tf*(-k1*xA**2)
 	f3 = xA + xB + xC - 1
 	return [f1, f2, f3]
 f = func()
 m.Equation( xB.dt() == f[0])
 m.Equation( xA.dt() == f[1])
 m.Equation( 0 == f[2] )
+# m.Equation(xB >= 0.2)
 #Resolver um problema do tipo simulação dinamica
 #Setar o modo de operação
 m.options.IMODE = 6
@@ -65,20 +67,19 @@ custoT = m.Intermediate(0.01*abs(300-T))
 # CRIAR A FUNÇÃO OBJETIVO
 # MAX DE F = MIN DE -F
 # GEKKO SOMENTE OTIMIZA PARA O MINIMO
-m.Obj( -xB*final*precoB + custoT )
+m.Obj( -xB*final*10 + Tf )
 # chamar o solver -> método de solução
 m.solve(disp=False)
-print("Fobj = ", xB.value[-1]*precoB - custoT.value[-1] )
 #plotar gráficos
 plt.figure()
-plt.subplot(2,1,1)
-plt.plot( m.time, xB.value )
+# plt.subplot(2,1,1)
+plt.plot( m.time*Tf.value[-1], xB.value )
 plt.xlabel("Tempo")
 plt.ylabel("$x_B$")
 
-plt.subplot(2,1,2)
-plt.plot(m.time, T.value, "r")
-plt.ylabel("Temperatura")
-plt.xlabel("Tempo")
+# plt.subplot(2,1,2)
+# plt.plot(m.time, T.value, "r")
+# plt.ylabel("Temperatura")
+# plt.xlabel("Tempo")
 # plt.legend("Fobj = " + str[xB.value[-1]*precoB - custoT.value[-1]])
 plt.show()
